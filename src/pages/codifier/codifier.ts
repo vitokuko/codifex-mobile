b2fca3aad58af50330e3316154040acc6ea39e69
souaibou <ndiayevito04@gmail.com>
2018-01-15 03:33:53 +0000

    origin
    add component codifier

--- src/pages/codifier/codifier.ts
+++ src/pages/codifier/codifier.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Platform } from 'ionic-angular/platform/platform';
 
 /**
  * Generated class for the CodifierPage page.
@@ -15,7 +16,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CodifierPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  isAndroid : Boolean = false;
+  constructor(public navCtrl: NavController, public navParams: NavParams , public platform: Platform) {
+    this.isAndroid = platform.is('android');
   }
 
   ionViewDidLoad() {
