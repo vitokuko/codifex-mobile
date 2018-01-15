da4d31533927bcb5106274a886d668d3df7f9329
souaibou <ndiayevito04@gmail.com>
2018-01-15 03:37:25 +0000

    origin
    fixed: bug "promise error"

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
+import { NavParams } from 'ionic-angular/navigation/nav-params';
+import { ModalController } from 'ionic-angular/components/modal/modal-controller';
+import { DetailPavPage } from '../detail-pav/detail-pav';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  isAndroid: boolean = false;
+  listPavillon = [
+    {
+      libelle : "Pavillon A"
+    },
+    {
+      libelle: "Pavillon B"
+    },
+    {
+      libelle: "Pavillon C"
+    },
+    {
+      libelle: "Pavillon D"
+    },
+  ];
 
+  constructor(public navCtrl: NavController,public platform: Platform, public navParams: NavParams, public modalCtrl: ModalController) {
+    this.isAndroid = platform.is('android');
+  }
+
+  goDetailPav(pavillon){
+    let detailPav = this.modalCtrl.create(DetailPavPage , {pavillon : pavillon});
+    detailPav.onDidDismiss(data => {
+        console.log(data);
+    });
+    detailPav.present();
   }
 
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,6 +4,7 @@ import { ReserverPage } from '../reserver/reserver';
 import { CodifierPage } from '../codifier/codifier';
 import { HomePage } from '../home/home';
 import { SettingsPage } from '../settings/settings';
+import { Platform } from 'ionic-angular/platform/platform';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -14,8 +15,9 @@ export class TabsPage {
   tab2Root = CodifierPage;
   tab3Root = ReserverPage;
   tab4Root = SettingsPage;
-
-  constructor() {
-
+  
+  isAndroid : Boolean = false;
+  constructor(public platform: Platform) {
+      this.isAndroid = platform.is('android');
   }
 }
